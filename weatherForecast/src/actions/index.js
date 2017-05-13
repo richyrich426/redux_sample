9acd6d358a525b4e34193f5e4c50957391af1d34
Richard Levey <riclev123@gmail.com>
2017-05-13 20:37:12 +0200

    weather app started lecture 56

---
+++ weatherForecast/src/actions/index.js
@@ -0,0 +1,2 @@
+// riclev123@gmail.com 1-> 8
+const API_KEY = 'd8bef569ad5cab1575d5fec286f43848';
---
+++ weatherForecast/src/components/app.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import React from 'react'
+import SearchBar from '../containers/search_bar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <SearchBar />
+      </div>
+    );
+  }
+}
