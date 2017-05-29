3eb9119a5eef8d02ad7e97cfd04add09193ae2a2
Richard Levey <riclev123@gmail.com>
2017-05-29 20:16:46 +0200

    Creating a new post ui lecture -->128

--- react-router-example/src/index.js
+++ react-router-example/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom'
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
-import promise from 'redux-promise';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -15,6 +16,7 @@ ReactDOM.render(
     <BrowserRouter>
         <div>
             <Route path="/" component={PostsIndex} />
+            <Route path="/posts/new" component={PostsNew} />
             {/* <Route path="*" component={PostsIndex} /> */}
         </div>
     </BrowserRouter>
